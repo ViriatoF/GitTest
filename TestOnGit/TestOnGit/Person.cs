86ede259f0f21762af1c9af81e8fe9281a03e1f8
ViriatoF <https://github.com/ViriatoF>
2019-01-30 22:49:43 +0100

    Creation classe Person

---
+++ TestOnGit/TestOnGit/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOnGit
+{
+    public class Person
+    {
+        public int Identifiant { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+        public string Adresse { get; set; }
+
+        public Person(int aIdentifiant, string aNom, string aPrenom, int aAge, string aAdresse)
+        {
+            this.Identifiant = aIdentifiant;
+            this.Nom = aNom;
+            this.Prenom = aPrenom;
+            this.Age = aAge;
+            this.Adresse = aAdresse;
+        }
+    }
+}
