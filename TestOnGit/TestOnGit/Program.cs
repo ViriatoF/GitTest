366363e67f4928fb091100d9fc87c3c5e4620a1d
ViriatoF <https://github.com/ViriatoF>
2019-01-30 23:03:46 +0100

    Test avec 2 personnes

--- TestOnGit/TestOnGit/Program.cs
+++ TestOnGit/TestOnGit/Program.cs
@@ -10,6 +10,13 @@ namespace TestOnGit
     {
         static void Main(string[] args)
         {
+            Person Person1 = new Person(425, "Marfle", "Charles", 35, "12 rue de la troupenisse");
+            Person Person2 = new Person(426, "Plouckt", "Gonzague", 29, "66 rue de la perche dorée");
+            
+            Console.WriteLine(" Id : " +Person1.Identifiant+ "\n Nom : " +Person1.Nom+ "\n Prénom : " +Person1.Prenom+ "\n Age : " +Person1.Age+ "\n Adresse : " +Person1.Adresse);
+            Console.WriteLine("*******************************************************************************************************");
+            Console.WriteLine(" Id : " +Person2.Identifiant+ "\n Nom : " +Person2.Nom+ "\n Prénom : " +Person2.Prenom+ "\n Age : " +Person2.Age+ "\n Adresse : " +Person2.Adresse);
+            Console.ReadLine();
         }
     }
 }
