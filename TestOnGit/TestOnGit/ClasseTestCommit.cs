d5028f8493cf4d5fb5434738733557cf4ddad790
Vito <viriato.ferreira44@gmail.com>
2019-01-31 10:23:50 +0100

    Test de commit Classe soeur...( jeu de mots...)

---
+++ TestOnGit/TestOnGit/ClasseTestCommit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOnGit
+{
+    class ClasseTestCommit
+    {
+        // Ceci est une classe de test parcequ'elle le vaut bien... Comme l'Oréal mais en mieux...
+    }
+}
