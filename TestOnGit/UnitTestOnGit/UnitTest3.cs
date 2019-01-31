cb9ac982a4f2df36b019a27496c5c2f9abc315bf
Vito <viriato.ferreira44@gmail.com>
2019-01-31 10:21:23 +0100

    Pinpin le lapin

---
+++ TestOnGit/UnitTestOnGit/UnitTest3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOnGit
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Ce matin, un lapin, a tué un codeur...
+        }
+    }
+}
