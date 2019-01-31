bca655345a7c591b03360a2325a7549309e666e7
Vito <viriato.ferreira44@gmail.com>
2019-01-31 10:19:48 +0100

    Blouge, c'est bien blouge...

---
+++ TestOnGit/UnitTestOnGit/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOnGit
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Blouge, c'est bien blouge...
+        }
+    }
+}
